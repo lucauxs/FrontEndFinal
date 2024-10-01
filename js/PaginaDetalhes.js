f07616bdadc4bad5c8a609813f5ae7f82b5200bf
Guilherme <guilhermesenna2004@gmail.com>
2024-10-01 17:03:06 -0300

    JS iniciado

---
+++ js/PaginaDetalhes.js
@@ -0,0 +1,59 @@
+const produtos = [
+    {
+      id: 1,
+      nome: "Violão Gianini",
+      preco: 379.00,
+      descricao: "Um violão acessível com qualidade excelente para iniciantes ou músicos casuais. Sonoridade clássica e conforto na hora de tocar."
+    },
+    {
+      id: 2,
+      nome: "Baixo Waldman GBW",
+      preco: 967.00,
+      descricao: "Um baixo robusto com timbre poderoso e design moderno. Ideal para quem busca versatilidade em diversas estilos musicais."
+    },
+    {
+      id: 3,
+      nome: "Guitarra Fender",
+      preco: 818.00,
+      descricao: "A lendária Fender com som limpo e alta performance. Perfeita para músicos que buscam um instrumento clássico e confiável."
+    },
+    {
+      id: 4,
+      nome: "Saxofone Yamaha",
+      preco: 1290.00,
+      descricao: "Sonoridade rica e afinação impecável, perfeito para jazz, blues e música clássica. A Yamaha oferece durabilidade e precisão."
+    },
+    {
+      id: 5,
+      nome: "Kit Bateria Premium",
+      preco: 1976.00,
+      descricao: "Um kit completo e profissional, ideal para bateristas que procuram qualidade superior em cada batida. Som potente e componentes resistentes."
+    },
+    {
+      id: 6,
+      nome: "Clarinete NY 800",
+      preco: 1113.00,
+      descricao: "Clarinete com excelente projeção de som e construção sólida. Uma escolha ideal tanto para estudantes quanto para músicos avançados."
+    },
+    {
+      id: 7,
+      nome: "Mesa de Mixagem",
+      preco: 6299.00,
+      descricao: "Com qualidade de estúdio e múltiplas entradas, esta mesa oferece controle total sobre suas mixagens. Ideal para gravações e apresentações ao vivo."
+    },
+    {
+      id: 8,
+      nome: "Microfone Dinâmico Samson",
+      preco: 899.00,
+      descricao: "Captação de som clara e precisa, ideal para vocalistas e palestrantes. Compacto, durável e com excelente custo-benefício."
+    },
+    {
+      id: 9,
+      nome: "Kalimba 17 Teclas",
+      preco: 53.00,
+      descricao: "Um instrumento portátil e divertido com som suave e melódico. Ótimo para relaxar ou adicionar um toque especial às suas composições."
+    }
+  ];
+  
+
+  
